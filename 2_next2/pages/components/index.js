124d5d9346fd45dbcd19bb95c43ad8a37a6b34ce
vaseksindelaru <vaseksindelaru@gmail.com>
2023-05-17 15:29:16 +0200

    rutas casi,casi

---
+++ 2_next2/pages/components/index.js
@@ -0,0 +1,60 @@
+import Layout from "../Layout";
+import Link from "next/link";
+
+export default function GamesList({gamesNuevos}) {
+
+    console.log(gamesNuevos)
+    return (
+<Layout>
+    <div>
+        {
+        gamesNuevos.map((gam) => ( 
+        <div key={gam.id} className="text-white">
+       <Link href="games/[game]" as={`/games/${gam.ruta}`}>
+       
+        <p>{gam.title}</p>
+        <img src={gam.thumbnail} alt=''/>
+        
+        </Link>
+        </div>
+        ))
+        }
+        
+         
+    </div>
+    </Layout> 
+    )
+}
+
+
+export  const getServerSideProps = async (context) =>{ 
+  try {
+    const peticion = await fetch ("https://www.freetogame.com/api/games")
+     const games = await peticion.json()
+     
+  const gamesNuevos = games.map((gamNew) => {
+    
+    return {
+      ...gamNew,
+      ruta: gamNew.title.split(' ').join('-').toLowerCase()
+    }
+  })
+return {
+    props:{gamesNuevos: gamesNuevos},
+    } }
+    
+    catch (error) {
+    console.log(error)
+  }
+  }
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2_next2/pages/games/[game].js
@@ -0,0 +1,106 @@
+/*
+import Layout from "../Layout";
+import React from 'react'
+
+export default function Game() {
+
+  console.log()
+  return (
+    
+  <Layout>
+    <div className="text-white">
+    <h1>Game</h1>
+    </div>
+  </Layout>
+  )
+}
+
+export async function getStaticPaths(){
+  try {
+    const res = await fetch ("https://www.freetogame.com/api/games?category=shooter")
+    const datas = await res.json()
+    
+    const paths = datas.map((data)=>({
+      params:{id: data.id.toString()}
+    }))
+    console.log(paths)
+return{
+  paths,
+  fallback:false
+} } catch (error) {
+    console.log(error)
+  }
+}
+
+export  const getStaticProps = async ({params}) =>{ 
+  try {
+  const url = "https://www.freetogame.com/api/games?category=shooter/"+params.id
+  const resp = await fetch (url)
+const games = await resp.json()
+console.log (games)
+return {
+  props:{
+      games
+  }
+  
+} }catch (error) {
+  console.log(error)
+}
+}
+*/
+import Layout from "../Layout";
+import React from 'react'
+import {useRouter} from 'next/router'
+
+
+
+export default function Game({gameDiscower}) {
+  const router = useRouter()
+  console.log(router)
+  console.log(gameDiscower)
+  return (
+    
+  <Layout>
+    <div className="text-white">
+    {
+        gameDiscower.map((gam) => {
+          return(
+        <div key={gam.id}>
+
+        <p>{gam.title}</p>
+        <img src={gam.thumbnail} alt=''/>
+        </div>)
+})
+        }
+     </div>
+  </Layout>
+  )
+}
+
+export  const getServerSideProps = async ({query}) =>{ 
+  
+  
+
+  try {
+  const peticion = await fetch ("https://www.freetogame.com/api/games?category=shooter")
+   const games = await peticion.json()
+
+   console.log('query', query)
+const gamesNuevos = games.map((gamNew) => {
+
+  return {
+    ...gamNew,
+    ruta: gamNew.title.split(' ').join('-').toLowerCase()
+  }
+})
+
+const gameDiscower = gamesNuevos.filter((gam) => gam.ruta === query.game)
+console.log('gameDiscower', gameDiscower)
+
+return {
+  props:{gameDiscower: gameDiscower},
+  } }
+  catch (error) {
+  console.log(error)
+}
+}
